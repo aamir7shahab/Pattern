3bf3e9095c7134394ad41846b68457468c89904b
aamir7shahab <aamir.shahab007@gmail.com>
2020-07-09 02:22:20 +0530

    valdiation of lastname created

--- userReg.sh
+++ userReg.sh
@@ -8,3 +8,14 @@ if [[ $fname =~ $pat1 ]]; then
 else
 	echo "First name invalid."
 fi
+
+# uc2 (User need to enter a valid Last Name)
+read -p "Enter first name: " lname
+
+pat1="^[A-Z][a-z0-9]{3,}$"
+
+if [[ $lname =~ $pat1 ]]; then
+	echo "First name valid."
+else
+	echo "First name invalid."
+fi
